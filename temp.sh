1c010171e744b0f47531ca086cae05316b90f51f
votebmyla <votebmyla@gmail.com>
2022-12-21 17:59:08 +0500

    add task-2 solution

--- temp.sh
+++ temp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# Place your code here
+if [[ $1 == *K ]]; then
+  # removes K at the end, then adds C
+  echo "$((${1/%K/} - 273))C"
+else
+  # removes C at the end, then adds K
+  echo "$((${1/%C/} + 273))K"
+fi
