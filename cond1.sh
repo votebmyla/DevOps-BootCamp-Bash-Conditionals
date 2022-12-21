86875f003a5d645ff7bc834186a9341098f79bbf
votebmyla <votebmyla@gmail.com>
2022-12-21 16:24:52 +0500

    add task-1 solution

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-IFS=',' read -r -a array <<< "$1"
+IFS=',' read -r -a array <<<"$1"
 
-#Write your code here
+# sum of array
+sum=0
+
+# iterate array
+for e in "${array[@]}"; do
+  # check if element evem
+  if [[ $(($e % 2)) -eq 0 ]]; then
+    # add element
+    sum=$(($e + $sum))
+  fi
+done
+
+echo $sum
